66966a90787ef629850fa10255c7b311864eee07
Kyle6012 <bahatikylemeshack@gmail.com>
2024-07-20 02:45:08 +0300

    js

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-
-const {
-   spawn
-} = require('child_process')
+const { spawn } = require('child_process')
 const path = require('path')
 
-function start() {
-   let args = [path.join(__dirname, 'plugins.js'), ...process.argv.slice(2)]
-   console.log([process.argv[0], ...args].join('\n'))
-   let p = spawn(process.argv[0], args, {
-         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-      })
-      .on('message', data => {
-         if (data == 'reset') {
+// Function to start the server with the specified port
+function start(port) {
+    let args = [path.join(__dirname, 'plugins.js'), port, ...process.argv.slice(2)]
+    console.log(`Spawning process: ${process.argv[0]} ${args.join(' ')}`)
+
+    let p = spawn(process.argv[0], args, {
+        stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+    })
+
+    p.on('message', data => {
+        if (data === 'reset') {
             console.log('Restarting Bot...')
             p.kill()
-            start()
-            delete p
-         }
-      })
-      .on('exit', code => {
-         console.error('Exited with code:', code)
-         if (code == '.' || code == 1 || code == 0) start()
-      })
+            start(port) // Restart with the same port
+        }
+    })
+
+    p.on('exit', code => {
+        console.error('Exited with code:', code)
+        // Retry if the process exits with an error code or is terminated
+        if (code === 0 || code === 1) start(port) // Restart on exit code 0 or 1
+    })
 }
-start()
+
+// Default port or specify another one
+const port = process.env.PORT || 3000 // Use PORT environment variable or default to 3000
+start(port)
