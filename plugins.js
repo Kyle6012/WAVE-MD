ecdea64e1036a163a91c2a883041bbb97bc182df
Kyle6012 <bahatikylemeshack@gmail.com>
2024-07-21 03:18:49 +0000

    h

--- plugins.js
+++ plugins.js
@@ -55,7 +55,7 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
          let jid = jidNormalizedUser(key.remoteJid)
          let msg = await store.loadMessage(jid, key.id)
 
-         return msg?.message || ""
+         return msg.message || ""
       },
       msgRetryCounterCache, // Resolve waiting messages
       defaultQueryTimeoutMs: undefined, // for this issues https://github.com/WhiskeySockets/Baileys/issues/276
