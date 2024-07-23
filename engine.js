77d28f9eba7f09460d33c52a956da659d39d997f
Kyle6012 <bahatikylemeshack@gmail.com>
2024-07-23 11:44:23 +0000

    h

--- engine.js
+++ engine.js
@@ -436,7 +436,7 @@ const wavefeature = () =>{
             var numUpper = (mytext.match(/case '/g) || []).length
             return numUpper
 }
-  
+
             switch (command) {
             	
             case 'tutorial':{
@@ -1402,6 +1402,7 @@ break;
             }
             break;
             case 'checkdeath':
+             const axios = require('axios');
              if (!text) return replay(`Use Someone's Name, Example : ${prefix + command} Bealth`)
               predea = await axios.get(`https://api.agify.io/?name=${q}`)
               reply(`Name : ${predea.data.name}\n*Dead At Age :* ${predea.data.age} Year.\n\n_Quick, Quick, Repent Bro, Because No One Knows About Death_`)
@@ -1617,7 +1618,7 @@ break;
 
 case 'chatgpt':
       case 'gpt':
-      case 'chatbot':
+      case 'chatbot': {
        const axios = require("axios");
         if (!args[0]) {
           return reply(`Please provide a message to chat with the Wave chatbot. Example: ${prefix}chat How are you Wave ?`);
@@ -1634,6 +1635,7 @@ case 'chatgpt':
           console.error('Error fetching AI chatbot response:', error);
           reply('An error occurred while fetching the Wave chatbot response. Please try again later.');
         }
+      }
         break;
                
              case 'dalle': {
@@ -4884,9 +4886,9 @@ async function fetchScienceNewsHeadlines() {
 
 //Function of games
 case 'chat':
-    
+    const axios = require('axios');
     botreply = await axios.get(
-      `http://api.brainshop.ai/get?bid=180857&key=SeLyK3P24U91Ed7a&uid=[Wavebot]&msg=[text]`
+      `http://api.brainshop.ai/get?bid=180857&key=SeLyK3P24U91Ed7a&uid=[Mariabot]&msg=[text]`
     );
 
     txtChatbot = `${botreply.data.cnt}`;
